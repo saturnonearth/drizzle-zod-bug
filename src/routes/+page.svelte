a8fc2d8468bd06f6802175b806c6119bd0bde884
Cory Tyburski <corytyburski@gmail.com>
2023-04-17 19:26:17 -0400

    Create zod example

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,30 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+  import {
+    mysqlTable,
+    text,
+    serial,
+  } from 'drizzle-orm/mysql-core';
+  import { createInsertSchema } from 'drizzle-zod';
+  import { ZodObject } from 'zod';
+
+  export const users = mysqlTable('users', {
+    id: serial('id').primaryKey().notNull(),
+    name: text('name').notNull(),
+  });
+  
+  const insertSchema = createInsertSchema(users);
+  const isZodObject = insertSchema instanceof ZodObject;
+</script>
+
+<div>
+  Am I a ZOD Object? <span class:error={!isZodObject}>{isZodObject ? 'YES! 🤩' : 'No 😢'}</span>
+</div>
+
+<style>
+  span {
+    color: green;
+  }
+  .error {
+    color: red;
+  }
+</style>
